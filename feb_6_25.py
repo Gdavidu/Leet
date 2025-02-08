9aeb2d972c26dca4e4950d07df39e1a91e8db5f7
Gdavidu <david_gu@yahoo.com>
2025-02-07 23:53:46 -0800

    picking up?

---
+++ feb_6_25.py
@@ -0,0 +1,7 @@
+# TEST TEXT GIT PLS PICK UP
+print('why the hell does git not pick up these file insertions')
+PICKUP
+HEWLLffgit config user.name
+git config user.email
+tf is gogit --version
+ing on
