cceb9bad7a82f0fe30484db833e1dedf9c7646f5
gdavidu <david_gu@yahoo.com>
2025-02-11 23:07:41 -0800

    forgot about this one

--- feb_8_25.py
+++ feb_8_25.py
@@ -59,3 +59,29 @@ class Solution:
                 if nums.index(hit) == i:
                     return [i, nums.index(hit,i+1)]
                 return [i, nums.index(hit)]
+# Solution:
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}  # Stores {value: index}
+        
+        for i, num in enumerate(nums):
+            hit = target - num
+            if hit in num_dict:  # Check if the complement exists
+                return [num_dict[hit], i]
+            num_dict[num] = i  # Store the number with its index
+# Notes: Got caught up on the possibility of indexing for the matching value. Didn't occur to me
+# to even hashmap because I thought duplicates would mess up indexing. The solution is clean logic to me 
+# tho because its using the index (a unique calue) as a key, nvm the value is a key. 
+
+# Group Anagram
+# Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
