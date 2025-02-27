5b6e7bf66472499352e51ccf54efdeac7f832819
gdavidu <david_gu@yahoo.com>
2025-02-26 23:46:15 -0800

    better solution

--- arrays_and_hashing.py
+++ arrays_and_hashing.py
@@ -245,3 +245,28 @@ class Solution:
             res.append(s[i:i + sz])
             i += sz
         return res
+
+# Optimized Ver:
+class Solution:
+    
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "#" + s
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            res.append(s[i:j])
+            i = j
+            
+        return res
